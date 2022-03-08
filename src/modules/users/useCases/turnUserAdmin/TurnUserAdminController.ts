e109018b2fac094fd3bc793ab470fd640c81ab91
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-03-07 22:46:50 -0300

    feat: add treatment of error when user a non exists in TurnUserAdminController

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -8,9 +8,12 @@ class TurnUserAdminController {
   handle(request: Request, response: Response): Response {
     const { user_id } = request.params;
 
-    const user = this.turnUserAdminUseCase.execute({ user_id });
-
-    return response.send(user);
+    try {
+      const user = this.turnUserAdminUseCase.execute({ user_id });
+      return response.send(user);
+    } catch (error) {
+      return response.status(404).send({ error: error.message });
+    }
   }
 }
 
