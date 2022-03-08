99b13c4893312ba4ca499078c96e2c7e4b0a860f
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-03-07 23:03:51 -0300

    feat: implements a treatment for error in controller ListAllUsers

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -15,9 +15,12 @@ class ListAllUsersController {
   handle(request: Request, response: Response): Response {
     const { user_id } = request.headers;
 
-    const users = this.listAllUsersUseCase.execute({ user_id });
-
-    return response.send(users);
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id });
+      return response.send(users);
+    } catch (error) {
+      return response.status(400).send({ error: error.message });
+    }
   }
 }
 
