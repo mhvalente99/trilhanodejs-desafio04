c65812470d3154c9cc12c9a8f4b0555c0d9a2d81
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-03-07 22:58:52 -0300

    feat: implements a controller for list all user

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -2,11 +2,22 @@ import { Request, Response } from "express";
 
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
+declare module "http" {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  interface IncomingHttpHeaders {
+    user_id?: string;
+  }
+}
+
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    const users = this.listAllUsersUseCase.execute({ user_id });
+
+    return response.send(users);
   }
 }
 
