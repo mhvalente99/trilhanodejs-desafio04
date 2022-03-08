7a75ba834b6f836319aeb18ef442302f1b4ab980
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-03-07 21:53:15 -0300

    feat: implements a create new User in repository

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,34 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.users.push(user);
+
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    return undefined;
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    return undefined;
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    return receivedUser;
   }
 
   list(): User[] {
-    // Complete aqui
+    return [];
   }
 }
 
